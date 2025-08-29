fbbacd37b9ebefd486c7ab8cb4dab9e0185188e4
Christos Georgiadis <sonic9495@gmail.com>
2025-08-29 10:53:57 +0300

    feat: add livekit dashboard to the side bar

--- app/components/layout/full/vertical-sidebar/sidebarItem.ts
+++ app/components/layout/full/vertical-sidebar/sidebarItem.ts
@@ -1,47 +1,50 @@
 export interface menu {
-  header?: string;
-  title?: string;
-  icon?: any;
-  to?: string;
-  chip?: string;
-  BgColor?: string;
-  chipBgColor?: string;
-  chipColor?: string;
-  chipVariant?: string;
-  chipIcon?: string;
-  children?: menu[];
-  disabled?: boolean;
-  type?: string;
-  subCaption?: string;
-  external?: boolean;
+  header?: string
+  title?: string
+  icon?: any
+  to?: string
+  chip?: string
+  BgColor?: string
+  chipBgColor?: string
+  chipColor?: string
+  chipVariant?: string
+  chipIcon?: string
+  children?: menu[]
+  disabled?: boolean
+  type?: string
+  subCaption?: string
+  external?: boolean
 }
 
 const sidebarItem: menu[] = [
-  { header: "Home" },
+  { header: 'Home' },
   {
-    title: "Dashboard",
-    icon: "adhesive-plaster-outline",
-    to: "/",
+    title: 'Dashboard',
+    icon: 'adhesive-plaster-outline',
+    to: '/',
   },
-  { header: "Pages" },
+  { header: 'Pages' },
   {
-    title: "Sample Page",
-    icon: "planet-3-line-duotone",
-    to: "/sample-page",
-  },
-  { header: "auth" },
-  {
-    title: "Login",
-    icon: "login-3-line-duotone",
-    to: "/auth/login",
+    title: 'Sample Page',
+    icon: 'planet-3-line-duotone',
+    to: '/sample-page',
   },
   {
-    title: "Register",
-    icon: "user-plus-rounded-line-duotone",
-    to: "/auth/register",
+    title: 'LiveKit',
+    icon: 'planet-3-line-duotone',
+    to: '/livekit-dashboard',
   },
+  { header: 'auth' },
+  {
+    title: 'Login',
+    icon: 'login-3-line-duotone',
+    to: '/auth/login',
+  },
+  {
+    title: 'Register',
+    icon: 'user-plus-rounded-line-duotone',
+    to: '/auth/register',
+  },
+]
 
-
-];
-
-export default sidebarItem;
+export default sidebarItem
